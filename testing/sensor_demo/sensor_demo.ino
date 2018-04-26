b3ff5bfdde6230e8e950582088015c59afc7e10d
Nick McComb <nick@nickmccomb.net>
2018-04-25 20:52:04 -0700

    Added PSI interpret code

--- testing/sensor_demo/sensor_demo.ino
+++ testing/sensor_demo/sensor_demo.ino
@@ -1,29 +1,96 @@
+#define PRESSURE_INPUT_PIN A5
+#define VALVE_CONTROL_PIN 9
+#define COMPRESSOR_CONTROL_PIN 2
+
+#define LED_PIN 13
+
+String input;
+
 void setup() {
 
-  pinMode(2, OUTPUT);
-  digitalWrite(2, LOW);
-  pinMode(13, OUTPUT);
-  digitalWrite(13, LOW);
-  pinMode(A5, INPUT);
+  pinMode(COMPRESSOR_CONTROL_PIN, OUTPUT);
+  digitalWrite(COMPRESSOR_CONTROL_PIN, LOW);
+  pinMode(LED_PIN, OUTPUT);
+  digitalWrite(LED_PIN, LOW);
+  pinMode(PRESSURE_INPUT_PIN, INPUT);
+
+  pinMode(VALVE_CONTROL_PIN, OUTPUT);
+  digitalWrite(VALVE_CONTROL_PIN, LOW);
 
   Serial.begin(9600);
 }
 
 void loop() {
-  digitalWrite(2, LOW);
-  digitalWrite(13, LOW);
-  for(int i = 0; i < 250; ++i){
-    delay(20);
-    Serial.print("Read Analog Value: ");
-    Serial.println(analogRead(A5));
+  uint16_t targetPSI = 0;
+  
+  if(Serial.available()){
+    input = Serial.readStringUntil('\n');
+    Serial.flush();
+
+    if(isValidNumber(input)){
+      targetPSI = input.toInt();
+      
+      Serial.print("Target PSI: ");
+      Serial.println(targetPSI);
+
+      
+    }
+    
   }
-  digitalWrite(2, HIGH);
-  digitalWrite(13, HIGH);
-  for(int i = 0; i < 50; ++i){
-    delay(20);
+
+  //Turn off the compressor
+  digitalWrite(COMPRESSOR_CONTROL_PIN, LOW);
+  digitalWrite(LED_PIN, LOW);
+  for(int i = 0; i < 2; ++i){
+    delay(160);
     Serial.print("Read Analog Value: ");
-    Serial.println(analogRead(A5));
+    Serial.print(readPressure());
+    Serial.println(" psi");
+    Serial.flush();
   }
+
+  /*
+  //Turn on the compressor
+  digitalWrite(COMPRESSOR_CONTROL_PIN, HIGH);
+  digitalWrite(LED_PIN, HIGH);
+  for(int i = 0; i < 6; ++i){
+    delay(160);
+    Serial.print("Read Analog Value: ");
+    Serial.print(readPressure());
+    Serial.println(" psi");
+    Serial.flush();
+  }
+  */
 
 
 }
+
+/*
+ * 
+ * Pressure is a signal from 1 - 5 volts with 5 volts signifying 1 MPa 
+ * See http://nickmccomb.ddns.net:8090/display/AR/Sensors#Sensors-ISE50-T2-62L
+ */
+float readPressure(){
+  analogRead(PRESSURE_INPUT_PIN);
+  uint16_t raw_adc = analogRead(PRESSURE_INPUT_PIN);
+
+  float voltage = 0.004887585532746823 * (float) raw_adc;  //ADC count (10 bit) to voltage conversion
+
+  float pressure = (250000.0 * voltage -250000.0) * 0.000145038; //Convert voltage to pascals, then to PSI
+
+  return pressure;
+}
+
+
+boolean isValidNumber(String str){
+   for(byte i=0;i<str.length();i++)
+   {
+      if(isDigit(str.charAt(i))) return true;
+        }
+   return false;
+} 
+
+
+
+
+
