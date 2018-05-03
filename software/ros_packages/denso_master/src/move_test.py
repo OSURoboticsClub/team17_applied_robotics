c28f78bf084e238f70b4c364a6242542a143330e
Corwin Perren <caperren@gmail.com>
2018-05-03 11:44:02 -0700

    Changed move test to check new fire and test positions.

--- software/ros_packages/denso_master/src/move_test.py
+++ software/ros_packages/denso_master/src/move_test.py
@@ -6,21 +6,21 @@ import random
 
 rospy.init_node("another_test")
 
-pub = rospy.Publisher("/denso_control/relative_joints", Float32MultiArray, queue_size=10)
-speed_pub = rospy.Publisher("/denso_control/speed", UInt8, queue_size=10)
+pub = rospy.Publisher("/denso_control/absolute_joints", Float32MultiArray, queue_size=10)
 
 message = Float32MultiArray()
-speed = UInt8()
 
 while not rospy.is_shutdown():
 
 
-    message.data = (-20.0, 0.0, 0.0, 0.0, 0.0, 0.0)
+    message.data = (0, -70, 70, 0, 90, -5) # Catch
 
     pub.publish(message)
 
-    message.data = (20.0, 0.0, 0.0, 0.0, 0.0, 0.0)
+    time.sleep(3)
+
+    message.data = (0, -30, 135, 0, 29, -5) # Fire
 
     pub.publish(message)
 
-    time.sleep(1)
+    time.sleep(3)
